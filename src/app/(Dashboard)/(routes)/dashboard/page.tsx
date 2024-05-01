529c3efc07166171d59f489b76e84b8e1315d6e1
nimarhmti <nimarahmati2000@gmail.com>
2024-05-01 12:36:07 +0330

    test

--- src/app/(Dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(Dashboard)/(routes)/dashboard/page.tsx
@@ -1,14 +1,40 @@
+"use client";
 import React from "react";
+import { tools, toolsModel } from "./tools/tools";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const router = useRouter();
   return (
-    <div className="mb-8 space-y-4 pt-3">
-      <h2 className="text-2xl md:text-4xl font-bold text-center">
-        Explore the power of The AI
-      </h2>
-      <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-        Chat with the Smartest AI - Experience the power of the AI
-      </p>
+    <div>
+      <div className="mb-8 space-y-4 pt-3">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of The AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the Smartest AI - Experience the power of the AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool: toolsModel) => (
+          <Card
+            key={tool.href}
+            className="p-4 border-black/5 transition cursor-pointer hover:shadow-md flex items-center justify-between"
+            onClick={() => router.push(tool.href)}
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("w-fit p-2 rounded-md ", tool.bgColor)}>
+                <tool.icon className={cn(tool.color)} />
+              </div>
+              <p>{tool.label}</p>
+            </div>
+            <ArrowRight className="h-5 w-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- src/app/(Dashboard)/(routes)/dashboard/tools/tools.tsx
+++ src/app/(Dashboard)/(routes)/dashboard/tools/tools.tsx
@@ -8,51 +8,51 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
-interface toolsModel extends routesModel {
+export interface toolsModel extends routesModel {
   bgColor: string;
 }
-export const routes: toolsModel[] = [
-  {
-    label: "dashboard",
-    href: "/dashboard",
-    icon: LayoutDashboard,
-    color: "text-sky-500",
-    bgColor: "text-sky-500/10",
-  },
+export const tools: toolsModel[] = [
+  // {
+  //   label: "dashboard",
+  //   href: "/dashboard",
+  //   icon: LayoutDashboard,
+  //   color: "text-sky-500",
+  //   bgColor: "bg-sky-500/10",
+  // },
   {
     label: "Conversation",
     href: "/conversation",
     icon: MessageSquare,
     color: "text-violet-500",
-    bgColor: "text-violet-500/10",
+    bgColor: "bg-violet-500/10",
   },
   {
     label: "Image Generator",
     href: "/images",
     icon: ImageIcon,
     color: "text-pink-700",
-    bgColor: "text-pink-700/10",
+    bgColor: "bg-pink-700/10",
   },
   {
     label: "Video Generator",
     href: "/video",
     icon: VideoIcon,
     color: "text-orange-500",
-    bgColor: "text-orange-500/10",
+    bgColor: "bg-orange-500/10",
   },
   {
     label: "music Generator",
     href: "/music",
     icon: Music,
     color: "text-emerald-500",
-    bgColor: "text-emerald-500/10",
+    bgColor: "bg-emerald-500/10",
   },
   {
     label: "code Generator",
     href: "/code",
     icon: Code,
     color: "text-green-700",
-    bgColor: "text-green-700/10",
+    bgColor: "bg-green-700/10",
   },
   //   {
   //     label: "Settings",
